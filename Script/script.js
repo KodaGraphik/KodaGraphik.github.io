53a77206fd28b9aa47ac3c2066e1c6369c6067de
KodaGraphik <40672672+KodaGraphik@users.noreply.github.com>
2019-02-03 22:07:10 +0100

    V2.0 FULL REBUILD

---
+++ Script/script.js
@@ -0,0 +1,13 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script src="http://v4-alpha.getbootstrap.com/dist/js/bootstrap.min.js"></script>
+<script>
+  $(document).ready(function() {
+    $("#hamburger").click(function() {
+      $("#navigation").slideToggle("400");
+    });
+
+</script>
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+<script>
+  AOS.init();
+</script>
