eca469d9cf36d2874e82954fd0a4c978b2a48c92
KodaGraphik <40672672+KodaGraphik@users.noreply.github.com>
2019-02-04 21:02:39 +0100

    V2.05
    Footer Code

--- Script/script.js
+++ Script/script.js
@@ -1,13 +1,10 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-<script src="http://v4-alpha.getbootstrap.com/dist/js/bootstrap.min.js"></script>
 <script>
   $(document).ready(function() {
     $("#hamburger").click(function() {
-      $("#navigation").slideToggle("400");
+      $("#navigation").fadeToggle("fast");
+      $("#navcontainer").slideToggle("slow");
+      $("#menu").slideToggle("fast");
+      $("#close").slideToggle("fast");
     });
-
-</script>
-<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
-<script>
-  AOS.init();
+  });
 </script>
