f92513733e80169eb1b5d41b47a393018359d6b4
Rob Brown <Rob-b-07@live.co.uk>
2019-03-01 17:36:01 +0000

    V1.1.7
    Minor Bugfixes

--- Script/script.js
+++ Script/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   $('.navbg').hide(0)
   $('.hamburger').click(function(){
-    $('.navbg').fadeToggle(300);
-    $('.container-fluid').delay(300).toggleClass('grey');
+    $('.navbg').fadeToggle(200);
+    $('.container-fluid').delay(200).toggleClass('grey');
   });
   $('.navbg').click(function() {
     $('.navbg').hide();
