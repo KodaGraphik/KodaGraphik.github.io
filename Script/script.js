aeb2fa42b3a9463572c8628a8b3cbed5f550e17c
Rob Brown <Rob-b-07@live.co.uk>
2019-03-04 20:29:51 +0000

    V1.7.2
    Animation Fixes

--- Script/script.js
+++ Script/script.js
@@ -2,20 +2,22 @@ $(document).ready(function() {
 
   function open() {
     $('.nav').fadeToggle(300);
-    // $('.hamburger--container').toggle('drop', {direction: 'up'}, 150);
-    // $('.hamburger--closecontainer').delay(300).toggle('drop', {direction: 'left'}, 150);
+    $('.hamburger--closecontainer').toggle();
+    $('.hamburger--container').toggle();
     $('.hamburger').one("click", close);
 }
 
 function close() {
   $('.nav').fadeToggle(300);
-  // $('.hamburger--closecontainer').toggle('drop', {direction: 'up'}, 150);
-  // $('.hamburger--container').delay(300).toggle('drop', {direction: 'left'}, 150);
+  $('.hamburger--closecontainer').toggle();
+  $('.hamburger--container').toggle();
     $('.hamburger').one("click", open);
 }
 
 $(".hamburger").one("click", open);
 $('.nav--container').click(function() {
     $('.nav').fadeOut(300);
+    $('.hamburger--closecontainer').toggle();
+    $('.hamburger--container').toggle();
 });
 });
