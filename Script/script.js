387419b8f4c341946587bc7dfc4a369ada743552
Rob Brown <Rob-b-07@live.co.uk>
2019-03-04 20:15:25 +0000

    V1.7.0
    Nav Fix

--- Script/script.js
+++ Script/script.js
@@ -2,16 +2,20 @@ $(document).ready(function() {
 
   function open() {
     $('.nav').fadeToggle(200);
-    $('.hamburger--container').toggle('drop', {direction: 'up'}, 150);
-    $('.hamburger--closecontainer').delay(300).toggle('drop', {direction: 'left'}, 150);
-    $(this).one("click", close);
+    // $('.hamburger--container').toggle('drop', {direction: 'up'}, 150);
+    // $('.hamburger--closecontainer').delay(300).toggle('drop', {direction: 'left'}, 150);
+    $('.hamburger').one("click", close);
 }
 
 function close() {
   $('.nav').fadeToggle(200);
-  $('.hamburger--closecontainer').toggle('drop', {direction: 'up'}, 150);
-  $('.hamburger--container').delay(300).toggle('drop', {direction: 'left'}, 150);
-    $(this).one("click", open);
+  // $('.hamburger--closecontainer').toggle('drop', {direction: 'up'}, 150);
+  // $('.hamburger--container').delay(300).toggle('drop', {direction: 'left'}, 150);
+    $('.hamburger').one("click", open);
 }
+
 $(".hamburger").one("click", open);
+$('.nav--container').click(function() {
+  $('.nav').fadeToggle(200);
+});
 });
