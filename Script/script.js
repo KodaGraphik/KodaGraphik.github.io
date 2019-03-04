b67aa912737b77202e71226ba3aff9fd73aa495d
Rob Brown <Rob-b-07@live.co.uk>
2019-03-04 16:58:35 +0000

    Update To Nav

--- Script/script.js
+++ Script/script.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
-  $('.hamburger').click(function() {
+
+  function open() {
     $('.nav').fadeToggle(200);
-    // $('.hamburger--item').text('⤬')
-  });
+    $('.hamburger--container').toggle('drop', {direction: 'up'}, 150);
+    $('.hamburger--closecontainer').delay(300).toggle('drop', {direction: 'left'}, 150);
+    $(this).one("click", close);
+}
+
+function close() {
+  $('.nav').fadeToggle(200);
+  $('.hamburger--closecontainer').toggle('drop', {direction: 'up'}, 150);
+  $('.hamburger--container').delay(300).toggle('drop', {direction: 'left'}, 150);
+    $(this).one("click", open);
+}
+$(".hamburger").one("click", open);
 });
