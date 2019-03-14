ee51941f2ec42457cbc9d60ed16daef796a7f446
Rob Brown <Rob-b-07@live.co.uk>
2019-03-14 13:12:52 +0000

    V2.0

--- Script/script.js
+++ Script/script.js
@@ -4,14 +4,20 @@ $(document).ready(function() {
     $('.nav').fadeToggle(300);
     $('.navbar-toggler').one("click", close);
 }
-
 function close() {
   $('.nav').fadeToggle(300);
     $('.navbar-toggler').one("click", open);
 }
-
 $(".navbar-toggler").one("click", open);
 $('.nav--container').click(function() {
     $('.nav').fadeOut(300);
+});
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".tophero");
+    var $navlogo = $('.tophero--logo');
+    $nav.toggleClass('tophero-scrolled', $(this).scrollTop() > $nav.height());
+    $navlogo.toggleClass('tophero--logo-scrolled', $(this).scrollTop() > $nav.height());
+  });
 });
 });
