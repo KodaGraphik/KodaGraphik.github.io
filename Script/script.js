89f5fe5532068c34eb2b95080310a8bdc9699378
Rob Brown <Rob-b-07@live.co.uk>
2019-12-12 20:23:13 +0000

    scroll fix V1.01

--- Script/script.js
+++ Script/script.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
     var target = this.hash,
       $target = $(target);
     $('html, body').stop().animate({
-      'scrollTop': $target.offset().top - 140
+      'scrollTop': $target.offset().top - 240
     }, 800, 'swing', function() {
       window.location.hash = target;
     });
