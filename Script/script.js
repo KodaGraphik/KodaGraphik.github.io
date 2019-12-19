bb1c7900df5c6861d7145c2b130c0ad392b5eac9
Rob Brown <Rob-b-07@live.co.uk>
2019-12-19 21:43:20 +0000

    V1.2 Structure Rebuild

--- Script/script.js
+++ Script/script.js
@@ -1,34 +1,44 @@
-$(document).ready(function() {
-  // $('.scroll').hide();
-  $('.navigation').slideDown(400, 'swing');
+$(document).ready(function(){
+    $('.pagecontent').fadeIn(500, 'swing');
+	    $('.page-footer').fadeIn(500, 'swing');
+	    // $('.home-container').fadeIn(500, 'swing');
 
 
-    // $('.homelogo').removeClass('biggerlogo');
-  // $('.navigation').hide();
-  // $('.navigation').fadeIn(200);
 
-  $('a[href^="#"]').on('click', function(e) {
+	$('.navbutton').click(function() {
+    event.preventDefault();
+    newLocation = ($(this).find('a:first').attr('href'));
+		console.log(newLocation);
+    $('.pagecontent').fadeOut(500, 'swing');
+    $('.page-footer').fadeOut(500, 'swing');
+    $('.homepage').fadeOut(500, 'swing');
+    $.when($('.pagecontent').fadeOut())
+      .then(function() {
+        setTimeout(function() {
+          window.location = newLocation;
+        }, 0);
+      });
+  });
+	$('a[href^="#"]').on('click', function(e) {
     // e.preventDefault();
     var target = this.hash,
       $target = $(target);
     $('html, body').stop().animate({
-      'scrollTop': $target.offset().top - 240
+      'scrollTop': $target.offset().top - 140
     }, 800, 'swing', function() {
       window.location.hash = target;
     });
   });
 
-  // Click Nav Button to Fade Out
-  $('.navbutton').click(function() {
+  $('.brand').click(function() {
     event.preventDefault();
-    newLocation = this.href;
-    $('body').css('overflow','hidden')
-    $('.bottomfooter').fadeOut(400, 'swing');
-        $('.navigation').slideUp(400, 'swing');
-    $('.fadecontainer').fadeOut(400, 'swing');
-    $('::-webkit-scrollbar').css('opacity', '0');
-  window.scrollTo({top: 600, behavior: 'smooth'});
-    $.when($('.fadecontainer').fadeOut())
+    newLocation = ($(this).find('a:first').attr('href'));
+		console.log(newLocation);
+    $('.pagecontent').fadeOut(500, 'swing');
+    $('.page-footer').fadeOut(500, 'swing');
+    $('.navigation').fadeOut(500, 'swing');
+    $('.navigation_buttons').fadeOut(500, 'swing');
+    $.when($('.pagecontent').fadeOut())
       .then(function() {
         setTimeout(function() {
           window.location = newLocation;
@@ -36,36 +46,4 @@ $(document).ready(function() {
       });
   });
 
-  // Home Button Click to Fade Out
-  $('.homebutton').click(function() {
-    event.preventDefault();
-    newLocation = this.href;
-    $(document.body).addClass('black-body');
-      $('.navigation').slideUp(400, 'swing');
-    $('.bottomfooter').fadeOut(400, 'swing');
-    $('.fadecontainer').fadeOut(400, 'swing');
-      window.scrollTo({top: 400, behavior: 'smooth'});
-    $.when($('.fadecontainer').fadeOut())
-      .then(function() {
-        setTimeout(function() {
-          window.location = newLocation;
-        }, 0);
-      });
-  });
-
-
-  // $(window).scroll(function() {
-  //   if ($(this).scrollTop() > 100) {
-  //     $('.scroll').fadeIn();
-  //   } else {
-  //     $('.scroll').fadeOut();
-  //   }
-  // });
-  // $('.scroll').click(function() {
-  //   window.scroll({
-  //     top: 0,
-  //     left: 0,
-  //     behavior: 'smooth'
-  //   });
-  // });
 });
